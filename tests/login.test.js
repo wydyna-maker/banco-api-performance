b84ac80bf94767e992f093d098fc63c172391d05
Wydyna Hayala <wydyna@gmail.com>
2026-09-12 17:36:21 -0300

    inserindo stages

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,9 +2,13 @@ import http from 'k6/http'
 import { sleep, check } from 'k6'
 
 export const options = {
-   // iterations: 50, // essa linha foi removida para adicao dos Virtual user
-    vus: 10,
-    duration: '30s',
+   stages: [
+      { duration: '10s', target: 10 },
+      { duration: '20s', target: 10 },
+      { duration: '10s', target: 30 },
+      { duration: '20s', target: 30 },
+      { duration: '20s', target: 0 },
+   ],
     thresholds: {
       http_req_duration: ['p(90)<3000','max<5000'],
       http_req_failed: ['rate<0.01']
