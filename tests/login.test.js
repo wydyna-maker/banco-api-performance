86d11b492e04b0c3309fd236ef97aae227f29d67
Wydyna Hayala <wydyna@gmail.com>
2026-09-12 17:01:08 -0300

    Adicionei Vus e Duration para execucao dos testes

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,9 +2,11 @@ import http from 'k6/http'
 import { sleep, check } from 'k6'
 
 export const options = {
-    iterations: 50,
+   // iterations: 50, // essa linha foi removida para adicao dos Virtual user
+    vus: 10,
+    duration: '30s',
     thresholds: {
-      http_req_duration: ['p(90)<10','max<1'],
+      http_req_duration: ['p(90)<3000','max<5000'],
       http_req_failed: ['rate<0.01']
     }
 };
